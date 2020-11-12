f2eefd17bcd4cef9e2200e12dcf8157ed5db884b
hankelsesser <72819191+hankelse@users.noreply.github.com>
2020-11-12 16:58:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ values = [1, 2, 5, 10, 20, 50, 100, 200]
 coin_bag = []
 for i in range(len(values)):
     for n in range(200/values[i]):
-        coin_bag.append(i)
+        coin_bag.append(values[i])
 print(coin_bag)
