871f2725eb73f64ce609c9b31a0182a5b251fbd9
hankelsesser <72819191+hankelse@users.noreply.github.com>
2020-11-12 17:05:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+import itertools
 values = [1, 2, 5, 10, 20, 50, 100, 200]
-coin_bag = []
-for i in range(len(values)):
-    for n in range(200/values[i]):
-        coin_bag.append(values[i])
-print(coin_bag)
+
+def make_bag(values):
+    coin_bag = []
+    for i in range(len(values)):
+        for n in range(200/values[i]):
+            coin_bag.append(values[i])
+    return(coin_bag)
+
+coins = make_bag(values)
+combinations = []
+for length in range(len(coins)):
+    combinations.append(itertools.combinations(coins, length))
+print(combinations)
